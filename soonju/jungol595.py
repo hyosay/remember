6085d4492cc207866258d34d74c3b23881ba865d
soonjuLee <soonju0304@cu.ac.kr>
2021-12-20 19:54:53 +0900

    jongol 출력

--- soonju/jungol595.py
+++ soonju/jungol595.py
@@ -1,9 +1,88 @@
 '''a = input()
 print(a*2)'''
 
-
+'''
 a, b = map(str, input().split())
 if len(a) > len(b):
     print(len(a))
 else:
-    print(len(b))
+    print(len(b))'''
+
+'''a = list(map(str, input().split()))
+a.reverse()
+print(a.)'''
+
+'''a = list(map(int, input().split()))
+print(a[0]+a[2]+a[4])'''
+
+'''a = list(map(int, input().split()))
+b = a[0::2]
+c = a[1::2]
+print("odd : %d" %sum(b))
+print("even : %d" %sum(c))'''
+
+'''n = int(input())
+def a(n):
+    for i in range(n):
+        print("~!@#$^&*()_+|")
+        return n
+print(a)'''
+
+'''def add(x):
+    result = 0
+    for i in range(1, x+1):
+        result += i
+    return result
+print(add(100))'''
+
+'''def sibal(x):
+    for i in range(x+1):
+        for j in range(x):'''
+
+
+'''import numpy as np
+a = list(map(int, input().split()))
+
+print(sum(np.abs(a)))'''
+
+'''a,b = map(int, input().split())
+def sq(a,b):
+    c = abs(a**2 - b**2)
+    return c
+
+print(sq(a,b))'''
+
+'''N = int(input())
+for _ in range(N):
+    a = map(int, input().split())
+    b = a.sort(reverse=True)
+print(b)'''
+
+'''a = [0 for i in range(4)]
+n = int(input())
+while n !=0:
+    if n >=500:
+        a[0]+=1
+        n -= 500
+    elif n >=100:
+        a[1] +=1
+        n -=100
+    elif n >=50:
+        a[2] +=1
+        n -= 50
+    elif n >=10:
+        a[3] +=1
+        n -= 10
+print(a[0], a[1], a[2], a[3])'''
+
+n,m,k = map(int, input().split())
+
+if n+k < m:
+    print('condition error')
+else:
+    a = list(map(int, input().split()))
+    if len(a) == 0 or :
+        print("condition error")
+    else:
+        a = sorted(a, reverse=True)
+
---
+++ soonju/language coder출력.py
@@ -0,0 +1,56 @@
+'''501
+a = "Fun Programming!"
+print(a)'''
+'''502
+a = "Programming! It's fun."
+print(a)'''
+'''503
+a = "My name is Hong Gil Dong.\nI am 13 years old."
+print(a)'''
+'''504
+a = "(@) (@)\n(=^.^=)\n(-m-m-)"
+print(a)'''
+'''505
+a = "I can program well.\nDreams come true."
+print(a)'''
+'''506
+a = "My height\n170\nMy weight\n68.600000"
+print(a)'''
+'''507
+a = "5 Dan\n5 * 2 = 10"
+print(a)'''
+'''508
+print("      item     count     price")
+print("       pen        20       100")
+print("      note         5        95")
+print("    eraser       110        97")'''
+'''101
+a = "My name is Hong"
+print(a)'''
+'''102
+a = "My hometown\nFlowering mountain"
+print(a)'''
+'''103
+print("T"*10)
+print("T"*10)
+for i in range(3):
+    print("    TT    ")'''
+'''104
+kor = 90
+mat = 80
+eng = 100
+sum = kor+mat+eng
+avg = sum/3
+print("kor %d" %kor)
+print("mat %d" %mat)
+print("eng %d" %eng)
+print("sum %d" %sum)
+print("avg %d" %avg)'''
+'''105
+print("          Seoul     10,312,545        +91,375")
+print("          Pusan      3,567,910         +5,868")
+print("        Incheon      2,758,296        +64,888")
+print("          Daegu      2,511,676        +17,230")
+print("        Gwangju      1,454,636        +29,774")'''
+
+
